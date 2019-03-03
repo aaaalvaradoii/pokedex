96ee8d56edbae44397faefdfb1fae8ea9143a3b4
aaaalvaradoii <aaaalvaradoii@gmail.com>
2019-03-03 00:36:24 -0600

    Redesigned Pokemon entry cards and modal

---
+++ src/Pokemon.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import PokemonProfile from './PokemonProfile';
+import './index.css';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Divider from '@material-ui/core/Divider';
+import Card from '@material-ui/core/Card';
+
+class Pokemon extends Component{
+
+    constructor(props){
+        super(props);
+  
+        this.state = {
+            open: false,
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    };
+    
+    handleClose = () => {
+        this.setState({
+            open: false
+        });
+    };
+
+    render(){
+        const { pokemon, id } = this.props;
+        console.log('props: ',this.props);
+        let image = `../public/sprites/${id}.png`;
+        console.log('img: ',image);
+        let pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+        return (
+            <div>
+            <Card className="card">
+                <CardActionArea onClick={this.handleClickOpen}> 
+                <CardHeader 
+                    avatar={
+                        <Avatar aria-label="pokedex number">
+                            {id}
+                        </Avatar>
+                    }
+                    title={pokemonName}
+                />
+                <Divider variant="middle" />
+                <div className="pokemon-species">
+                <div className="pokemon-species-container">
+                    <div className="pokemon-species-sprite">
+                    <img src={require(`../public/sprites/${id}.png`)} alt={`${pokemon.name}`}/>
+                    </div>
+                </div>
+                </div>
+                </CardActionArea>
+            </Card>
+            <Dialog
+                open={this.state.open}
+                onClose={this.handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    #{`${id}`} {`${pokemonName}`}
+                </DialogTitle>
+                <Divider variant="middle" />
+                <PokemonProfile selectedPokemon={`${pokemon.name}`} handleClose={this.handleClose}/>
+            </Dialog>
+            </div>
+        ); 
+    }
+}
+
+export default Pokemon;
---
+++ src/PokemonList.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Pokemon from './Pokemon';
+
+class PokemonList extends Component{
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            species : [],
+            fetched : false,
+            loading : false,
+      }
+    }
+
+    componentWillMount() {
+        this.setState({
+            loading : true
+        });
+
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(res => res.json())
+        .then(response => {
+            this.setState({
+            species : response.results,
+            loading : true,
+            fetched : true
+        });
+        });
+    }
+  
+    render() {
+        const { fetched, species } = this.state;
+    //   let content;
+
+    //   let content = fetched ? 
+
+    //   if(fetched){
+    //     content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
+    //   }
+    //   else if (loading && !fetched) {
+    //       content = <p>Loading ...</p>;
+    //   }
+    //   else {
+    //     content = <div/>;
+    //   }
+
+      return (
+        <div className="container">
+            { fetched ? (
+                <div className="pokemon-species-list"> {
+                    species.map((pokemon, i) => (
+                        <Pokemon key={pokemon.name} id={i+1} pokemon={pokemon}/>)
+                    )
+                }
+                </div>
+            ) : (
+                <p>Loading ...</p>
+            )}
+            {/* {content} */}
+        </div>
+      );
+    }
+  }
+
+export default PokemonList;
