74967a7794d0bfb11ea827106d65a29a84c73748
aaaalvaradoii <aaaalvaradoii@gmail.com>
2019-03-04 00:54:03 -0600

    Updates

---
+++ precache-manifest.58e61887bc4a85dd9356aa98bc554eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.c92f8f4b.js"
+  },
+  {
+    "revision": "838bd58e0e911776aaac",
+    "url": "/pokedex/static/js/main.838bd58e.chunk.js"
+  },
+  {
+    "revision": "3052d7ee3db265072528",
+    "url": "/pokedex/static/js/2.3052d7ee.chunk.js"
+  },
+  {
+    "revision": "838bd58e0e911776aaac",
+    "url": "/pokedex/static/css/main.94a32d75.chunk.css"
+  },
+  {
+    "revision": "80a76984f1ac8895ac4f53895af39038",
+    "url": "/pokedex/index.html"
+  }
+];
